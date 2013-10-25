78dba8a538994eecfcca53c4942fc9d5589bd5cb
Houston Golden <houstongolden@gmail.com>
2013-10-25 15:24:14 -0700

    fixed development config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+Civly::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
