7fe2d35efc5891df1aa98d6105af6e17f76cfed0
Houston Golden <houstongolden@gmail.com>
2013-10-25 15:17:03 -0700

    changed some config files to try and fix deployed login on civly

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 Civly::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+ # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -34,3 +34,4 @@ Civly::Application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
+
