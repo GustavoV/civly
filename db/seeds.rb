13dd7cdc278ea3d40f327f5ceab543977306b664
Houston Golden <houstongolden@gmail.com>
2013-10-29 16:39:30 -0700

    fix my heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@user = User.create(username: 'ryanfrancis', email: 'ryan@example.com', password: 'password', password_confirmation: 'password')
+@user.save
