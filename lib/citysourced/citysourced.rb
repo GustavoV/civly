8fecbe112207b94a7780c381701d2efe7435c508
Jeffects <jeffreyleung@alumni.calpoly.edu>
2013-12-07 16:15:24 -0800

    added a class for citysourced and added several gems for security

---
+++ lib/citysourced/citysourced.rb
@@ -0,0 +1,36 @@
+
+class Citysourced
+	@base_uri = 'api.citysourced.com'
+
+	def initialize(attributes = {})
+		@api_key = attributes[:api_key]
+		@api_request_type = attributes[:api_request_type]
+		@api_request_version = attributes[:api_request_version]
+		@address = attributes[:address]
+		@radius = attributes[:radius]
+		@max_results = attributes[:max_results]
+		@include_details = attributes[:include_details]
+		@date_range_start = attributes[:date_range_start]
+		@date_range_end = attributes[:date_range_end]
+		@current_status = attributes[:current_status]
+	end
+
+	def to_hash
+		{
+			ApiAuthKey: @api_key,
+			ApiRequestType: @api_request_type,
+			ApiRequestVersion: @api_request_version,
+			Address: @address,
+			Radius: @radius,
+			MaxResults: @max_results,
+			IncludeDetails: @include_details,
+			DateRangeStart: @date_range_start,
+			DateRangeEnd: @date_range_end,
+			CurrentStatus: @current_status
+		}
+	end
+
+	def to_params
+		self.to_hash.to_xml(root: 'CsRequest')
+	end
+end
